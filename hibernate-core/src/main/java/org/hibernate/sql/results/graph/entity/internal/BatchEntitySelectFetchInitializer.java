8b42212c0855d8afffde5d85a4cbc32c6de3cc28
g <g@m25.home>
2026-09-08 14:37:18 +0200

    Make ParentInfo a value class and use an typed array

--- hibernate-core/src/main/java/org/hibernate/sql/results/graph/entity/internal/BatchEntitySelectFetchInitializer.java
+++ hibernate-core/src/main/java/org/hibernate/sql/results/graph/entity/internal/BatchEntitySelectFetchInitializer.java
@@ -4,9 +4,13 @@
  */
 package org.hibernate.sql.results.graph.entity.internal;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.ConcurrentModificationException;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+import java.util.function.Consumer;
 
 import org.hibernate.engine.spi.EntityKey;
 import org.hibernate.metamodel.mapping.AttributeMapping;
@@ -29,7 +33,7 @@ public class BatchEntitySelectFetchInitializer extends AbstractBatchEntitySelect
 	protected final Type referencedModelPartType;
 
 	public static class BatchEntitySelectFetchInitializerData extends AbstractBatchEntitySelectFetchInitializerData {
-		private HashMap<EntityKey, List<ParentInfo>> toBatchLoad;
+		private HashMap<EntityKey, ParentInfoList> toBatchLoad;
 
 		public BatchEntitySelectFetchInitializerData(
 				BatchEntitySelectFetchInitializer initializer,
@@ -68,7 +72,7 @@ public class BatchEntitySelectFetchInitializer extends AbstractBatchEntitySelect
 			toBatchLoad = data.toBatchLoad = new HashMap<>();
 		}
 		// Always register the entity key for resolution
-		final var parentInfos = toBatchLoad.computeIfAbsent( data.entityKey, key -> new ArrayList<>() );
+		final var parentInfos = toBatchLoad.computeIfAbsent( data.entityKey, key -> new ParentInfoList() );
 		// But only add the parent info if the parent entity is not already initialized
 		if ( owningData.getState() != State.INITIALIZED ) {
 			final var parentAttribute =
@@ -83,13 +87,140 @@ public class BatchEntitySelectFetchInitializer extends AbstractBatchEntitySelect
 		}
 	}
 
-	private static class ParentInfo {
+	private static value class ParentInfo {
 		private final Object parentInstance;
-		private final int propertyIndex;
+		private final short propertyIndex;
 
 		public ParentInfo(Object parentInstance, int propertyIndex) {
 			this.parentInstance = parentInstance;
-			this.propertyIndex = propertyIndex;
+			this.propertyIndex = (short) propertyIndex;
+		}
+	}
+
+	private static class ParentInfoList implements Iterable<ParentInfo> {
+		private static final int DEFAULT_CAPACITY = 10;
+
+		private static final ParentInfo[] DEFAULTCAPACITY_EMPTY_ELEMENTDATA = {};
+
+		private ParentInfo[] elementData;
+
+		private int size;
+
+		private int modCount = 0;
+
+		ParentInfoList() {
+			this.elementData = DEFAULTCAPACITY_EMPTY_ELEMENTDATA;
+		}
+
+		void add(ParentInfo e) {
+			add(e, elementData, size);
+		}
+
+		void add(ParentInfo e, ParentInfo[] elementData, int s) {
+			if (s == elementData.length)
+				elementData = grow();
+			elementData[s] = e;
+			size = s + 1;
+		}
+
+		public Iterator<ParentInfo> iterator() {
+			return new Itr();
+		}
+
+		private ParentInfo[] grow() {
+			return grow(size + 1);
+		}
+
+		private ParentInfo[] grow(int minCapacity) {
+			int oldCapacity = elementData.length;
+			if (oldCapacity > 0 || elementData != DEFAULTCAPACITY_EMPTY_ELEMENTDATA) {
+				int newCapacity = newLength(oldCapacity,
+						minCapacity - oldCapacity, /* minimum growth */
+						oldCapacity >> 1           /* preferred growth */);
+				return elementData = Arrays.copyOf(elementData, newCapacity);
+			} else {
+				return elementData = new ParentInfo[Math.max(DEFAULT_CAPACITY, minCapacity)];
+			}
+		}
+
+		public static final int SOFT_MAX_ARRAY_LENGTH = Integer.MAX_VALUE - 8;
+
+		public static int newLength(int oldLength, int minGrowth, int prefGrowth) {
+			// preconditions not checked because of inlining
+			// assert oldLength >= 0
+			// assert minGrowth > 0
+
+			int prefLength = oldLength + Math.max(minGrowth, prefGrowth); // might overflow
+			if (0 < prefLength && prefLength <= SOFT_MAX_ARRAY_LENGTH) {
+				return prefLength;
+			} else {
+				// put code cold in a separate method
+				return hugeLength(oldLength, minGrowth);
+			}
+		}
+
+		private static int hugeLength(int oldLength, int minGrowth) {
+			int minLength = oldLength + minGrowth;
+			if (minLength < 0) { // overflow
+				throw new OutOfMemoryError(
+						"Required array length " + oldLength + " + " + minGrowth + " is too large");
+			} else if (minLength <= SOFT_MAX_ARRAY_LENGTH) {
+				return SOFT_MAX_ARRAY_LENGTH;
+			} else {
+				return minLength;
+			}
+		}
+
+		private class Itr implements Iterator<ParentInfo> {
+			int cursor;       // index of next element to return
+			int lastRet = -1; // index of last element returned; -1 if no such
+			int expectedModCount = modCount;
+
+			// prevent creating a synthetic constructor
+			Itr() {}
+
+			public boolean hasNext() {
+				return cursor != size;
+			}
+
+			public ParentInfo next() {
+				checkForComodification();
+				int i = cursor;
+				if (i >= size)
+					throw new NoSuchElementException();
+				ParentInfo[] elementData = ParentInfoList.this.elementData;
+				if (i >= elementData.length)
+					throw new ConcurrentModificationException();
+				cursor = i + 1;
+				return elementData[lastRet = i];
+			}
+
+			public void remove() {
+				throw new IllegalStateException("NYE");
+			}
+
+			@Override
+			public void forEachRemaining(Consumer<? super ParentInfo> action) {
+				Objects.requireNonNull(action);
+				final int size = ParentInfoList.this.size;
+				int i = cursor;
+				if (i < size) {
+					final ParentInfo[] es = elementData;
+					if (i >= es.length)
+						throw new ConcurrentModificationException();
+					for (; i < size && modCount == expectedModCount; i++)
+						action.accept(elementData[i]);
+					// update once at end to reduce heap write traffic
+					cursor = i;
+					lastRet = i - 1;
+					checkForComodification();
+				}
+			}
+
+			final void checkForComodification() {
+				if (modCount != expectedModCount)
+					throw new ConcurrentModificationException();
+			}
 		}
 	}
 
